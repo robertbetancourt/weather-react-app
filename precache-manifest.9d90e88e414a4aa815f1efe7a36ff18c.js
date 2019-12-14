097aa0a88234a173efbfc4fce0c6c7834425d729
Robert <robertbetancourt212@gmail.com>
2019-12-14 16:17:05 -0400

    Updates

---
+++ precache-manifest.9d90e88e414a4aa815f1efe7a36ff18c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c2a2c3b207fb95b4ca9a15baef0599c",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "2aeebc5515e219862b39",
+    "url": "/weather-react-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "34f921cd70ef204ccd2d",
+    "url": "/weather-react-app/static/css/main.b1be668e.chunk.css"
+  },
+  {
+    "revision": "2aeebc5515e219862b39",
+    "url": "/weather-react-app/static/js/2.670fe1b0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-react-app/static/js/2.670fe1b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34f921cd70ef204ccd2d",
+    "url": "/weather-react-app/static/js/main.25b825bf.chunk.js"
+  },
+  {
+    "revision": "0035a0b0c2f4ec0272c6",
+    "url": "/weather-react-app/static/js/runtime-main.5df5440a.js"
+  },
+  {
+    "revision": "6a5af36c6858e2cc652a545edc1d939a",
+    "url": "/weather-react-app/static/media/background.6a5af36c.jpg"
+  },
+  {
+    "revision": "1066a309c74745808a460b21384c3428",
+    "url": "/weather-react-app/static/media/clouds.1066a309.png"
+  },
+  {
+    "revision": "b0f1109a8a8e67a6679fd265ce5ea0f4",
+    "url": "/weather-react-app/static/media/gauge.b0f1109a.png"
+  },
+  {
+    "revision": "54b8b20963a210b83201e7ad5a48fd6d",
+    "url": "/weather-react-app/static/media/rain.54b8b209.png"
+  },
+  {
+    "revision": "fbc6b3065d2fd23502ed30ddedf75f09",
+    "url": "/weather-react-app/static/media/rainSun.fbc6b306.png"
+  },
+  {
+    "revision": "9b1f317ee1e8840b27b989990f7e878a",
+    "url": "/weather-react-app/static/media/snowflake.9b1f317e.png"
+  },
+  {
+    "revision": "fb62048c2d84d38166fe8897e6d89ab4",
+    "url": "/weather-react-app/static/media/storm.fb62048c.png"
+  },
+  {
+    "revision": "c2f084d9cbe217a947917059e463b139",
+    "url": "/weather-react-app/static/media/sun.c2f084d9.png"
+  },
+  {
+    "revision": "48028025c5a0e6090366c3cd11f758cb",
+    "url": "/weather-react-app/static/media/sunCloud.48028025.png"
+  },
+  {
+    "revision": "c8ff9641bb0e0631eabfa51a8d3148ec",
+    "url": "/weather-react-app/static/media/water.c8ff9641.png"
+  }
+]);
